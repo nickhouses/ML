60b7fe69ce80e791b5af650122bf7cbd94f96524
nickhouses <149748280+nickhouses@users.noreply.github.com>
2024-04-23 19:09:59 -0700

    initial stuff

--- Project/project.py
+++ Project/project.py
@@ -1,3 +1,12 @@
 # Nicholas Casas, 2001901158
 # Brandon Timok, 
 # CS 422 - Final Project
+
+#imports
+import pandas as pd
+import numpy as np
+
+#read in csv file, skip first row since it's names? 
+data = pd.read_csv('pokemon.csv', header=1)
+
+
